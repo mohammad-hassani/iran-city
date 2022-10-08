9f4e63ba55f1dab156681e93be68d33591531ff4
Sajad Dehshiri <sajaddp71@gmail.com>
2022-10-08 23:14:19 +0330

    update json files.

--- src/modules/AllCities.ts
+++ src/modules/AllCities.ts
@@ -1,10 +1,10 @@
 // will return a list of all cities
-import * as cities from '../../json/cities.json';
+import * as cities from "../../list-of-cities-in-Iran/json/cities";
 
 export default function AllCities(): string[] {
-    let citiesArray:any = [];
-    for (let i = 0; i < Object.keys(cities).length-1; i++) {
-      citiesArray.push(cities[i]);
-    }
-    return citiesArray;
+  let citiesArray: any = [];
+  for (let i = 0; i < Object.keys(cities).length - 1; i++) {
+    citiesArray.push(cities[i]);
   }
+  return citiesArray;
+}
--- src/modules/AllProvinces.ts
+++ src/modules/AllProvinces.ts
@@ -1,10 +1,10 @@
 // will return a list of all provinces
-import * as provinces from '../../json/provinces.json';
+import * as provinces from "../../list-of-cities-in-Iran/json/provinces.json";
 
 export default function AllProvinces(): string[] {
-    let provincesArray:any = [];
-    for (let i = 0; i < Object.keys(provinces).length-1; i++) {
-        provincesArray.push(provinces[i]);
-    }
-    return provincesArray;
+  let provincesArray: any = [];
+  for (let i = 0; i < Object.keys(provinces).length - 1; i++) {
+    provincesArray.push(provinces[i]);
   }
+  return provincesArray;
+}
